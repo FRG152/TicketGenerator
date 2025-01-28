fe6c448c27629c1661fa989b0a82d20967b4f2c4
FRG152 <romanflavio3@hotmail.com>
2025-01-27 22:12:38 -0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
-import "aos/dist/aos.css";
-import AOS from "aos";
 import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-AOS.init();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
